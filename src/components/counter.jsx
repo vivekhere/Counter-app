0c927ba97b600296d123e80f77c1f459e79d6804
vivekhere <vivekjai10@gmail.com>
2021-01-12 20:05:29 +0530

    specifying children

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,12 +2,22 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   render() {
-    return <h1>Hello World</h1>;
-    // This is a jsx (javacript xml) notation
-    // react converts this to React.creatElement(...);
+    return (
+      // <div>
+      //   <h1>Hello World</h1>
+      //   <button>Increment</button>
+      // </div>
+      // this will wrap this div tag in the root id container
+      // wraping the h1 tag and a button in div is unnecessary
+
+      // Alternative
+      <React.Fragment>
+        <h1>Hello World</h1>
+        <button>Increment</button>
+      </React.Fragment>
+      // this will wrap the elements directly in the root id container
+    );
   }
 }
 
 export default Counter;
-
-// import this class in index.js
