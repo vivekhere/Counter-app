04c1989e4b4c8703a8f93bba9e8b077eb9a27e61
vivekhere <vivekjai10@gmail.com>
2021-01-12 19:52:06 +0530

    --- 1. Components --- first react component

---
+++ src/components/counter.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  render() {
+    return <h1>Hello World</h1>;
+    // This is a jsx (javacript xml) notation
+    // react converts this to React.creatElement(...);
+  }
+}
+
+export default Counter;
+
+// import this class in index.js
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.css";
+import Counter from "./components/counter";
+// because counter is the default export we don't need curly braces around it
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Counter />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
