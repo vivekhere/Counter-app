c61b8da13337bb624fe57d482f666c5ca990d8da
vivekhere <vivekjai10@gmail.com>
2021-01-12 20:30:53 +0530

    setting attributes 1. simple attribute

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,40 +1,30 @@
-// display values dynamically instead of hard coding
+// set attributes on elements
 
 import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
     count: 0,
+    imgageUrl: "https://picsum.photos/200",
+    // this will generate a random 200 x 200 image
   };
-  // state is a special property in react components
-  // it is an object which includes any data that this component needs
 
   render() {
     return (
       <div>
+        <img src={this.state.imgageUrl} alt="" />
         <span>{this.formatCount()}</span>
         <button>Increment</button>
       </div>
-      // this references the current object
-      // in between the curly braces we can write any valid javascript expression
-      // an expression is something that produces a value
-      // we can also call function that returns a value
+
+      // we cannnot set attributes in double quotes as it will be rendered as plain text
+      // instead we can define a property in the state object and use it
     );
   }
 
   formatCount() {
-    // return this.state.count === 0 ? "Zero" : this.state.count;
-    // we can improve the above code using object destructuring by removing repetition
-
     const { count } = this.state;
-    // return count === 0 ? "Zero" : count;
-
-    // we can also return a jsx expression as it gets compiled to react element
     return count === 0 ? <h3>Zero</h3> : count;
-
-    // jsx expressions are like a normal javascript objects
-    // we can return them or pass them to a function
-    // we can also use them as a const to a variable ex: const x = <h1>Hello</h1>
   }
 }
 
