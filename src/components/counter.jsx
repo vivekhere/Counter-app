ff286b1e9739198ceca2398de48f5b214ea6cf02
vivekhere <vivekjai10@gmail.com>
2021-01-12 20:47:23 +0530

    setting attributes 3. class and inline styles

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,4 +1,4 @@
-// set attributes on elements : class and styles
+// set attributes on elements : class and inline styles
 
 import React, { Component } from "react";
 
@@ -7,25 +7,16 @@ class Counter extends Component {
     count: 0,
   };
 
-  // in jsx we have a style attribute which can be used to style objects
-  // define a property called styles and reference it in jsx expression
-  // the properties of this object are css properties in camel case notation
-  styles = {
-    fontSize: 15, // react will convert this to '10px'
-    fontWeight: "bold",
-  };
-
   render() {
     return (
       <div>
-        <span style={this.styles} className="badge badge-primary m-2">
+        <span style={{ fontSize: 30 }} className="badge badge-primary m-2">
           {this.formatCount()}
         </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
-      // since these jsx expressions get compiled to plain js objects
-      // so we cannot use a class attribute because it is a reserved keyword in js
-      // instead we use className attribute
+
+      // for using inline styles we need double curly braces
     );
   }
 
