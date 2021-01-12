ae45aa8274514d627ff10870b8e62a4dedc33830
vivekhere <vivekjai10@gmail.com>
2021-01-12 20:23:19 +0530

    embedding expressions

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,22 +1,40 @@
+// display values dynamically instead of hard coding
+
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0,
+  };
+  // state is a special property in react components
+  // it is an object which includes any data that this component needs
+
   render() {
     return (
-      // <div>
-      //   <h1>Hello World</h1>
-      //   <button>Increment</button>
-      // </div>
-      // this will wrap this div tag in the root id container
-      // wraping the h1 tag and a button in div is unnecessary
-
-      // Alternative
-      <React.Fragment>
-        <h1>Hello World</h1>
+      <div>
+        <span>{this.formatCount()}</span>
         <button>Increment</button>
-      </React.Fragment>
-      // this will wrap the elements directly in the root id container
+      </div>
+      // this references the current object
+      // in between the curly braces we can write any valid javascript expression
+      // an expression is something that produces a value
+      // we can also call function that returns a value
     );
+  }
+
+  formatCount() {
+    // return this.state.count === 0 ? "Zero" : this.state.count;
+    // we can improve the above code using object destructuring by removing repetition
+
+    const { count } = this.state;
+    // return count === 0 ? "Zero" : count;
+
+    // we can also return a jsx expression as it gets compiled to react element
+    return count === 0 ? <h3>Zero</h3> : count;
+
+    // jsx expressions are like a normal javascript objects
+    // we can return them or pass them to a function
+    // we can also use them as a const to a variable ex: const x = <h1>Hello</h1>
   }
 }
 
