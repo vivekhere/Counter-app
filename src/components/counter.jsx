992f165d213adba3c72916f50f970c9662038b11
vivekhere <vivekjai10@gmail.com>
2021-01-13 19:33:16 +0530

    rendering lists

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,25 +1,36 @@
-// rendering classes dynamically
+// rendering lists
 
 import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
     count: 0,
+    tags: ["tag1", "tag2", "tag3"],
   };
 
-  render() {
-    // let classes = "badge m-2 badge-";
-    // classes += this.state.count === 0 ? "warning" : "primary";
-    // these two lines are purely about determining the class for className
-    // they are polluting the render method
-    // we can encapsulate these lines in seperate method getBadgeClasses
+  // to render the list of items
+  // we can use map method to map each element in the tags array into react element
 
+  render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </div>
     );
+    // we are getting a string 'tag' and mapping it to the jsx expression '<li>tag</li>'
+    // which will get compiled to the react element which is plain js object
+
+    // we need to provide a unique key to each element in the list
+    // this is because if the state of this react element changes react needs to quickly
+    // figure out what element is changed and where in the DOM it should make changes
+    // to make the DOM in sync with the virtual DOM.
+    // the key should be unique in this particular list.
   }
 
   getBadgeClasses() {
