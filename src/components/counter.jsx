151281508f1f092d66684005ac2dc508111642c2
vivekhere <vivekjai10@gmail.com>
2021-01-14 19:05:49 +0530

    passing event arguments - solution 2 better approach

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,7 @@
 // Passing event arguments
-// Solution 1
-
-// Sometimes in a real world application we have to pass arguments to our events handlers
-// but in onClick we need to pass a function reference so we cannot call handleIncrement()
-// and pass an argument
+// Solution 2
+// better approach
+// using inline function
 
 import React, { Component } from "react";
 
@@ -12,17 +10,9 @@ class Counter extends Component {
     count: 0,
   };
 
-  // pass an argument of any name of your choice or choose a name what the argument
-  // is representing
   handleIncrement = (product) => {
     console.log(product);
     this.setState({ count: this.state.count + 1 });
-  };
-
-  // we can define one function which will be used to pass an argument to our event
-  // handler and then reference our new function in onClick
-  doHandleIncrement = () => {
-    this.handleIncrement({ id: 1 });
   };
 
   render() {
@@ -30,7 +20,7 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.doHandleIncrement}
+          onClick={() => this.handleIncrement({ id: 1 })}
           className="btn btn-secondary btn-sm"
         >
           Increment
