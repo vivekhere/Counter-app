8fc1dbfec6aca0fe3150dca87fd1a6c5038a2024
vivekhere <vivekjai10@gmail.com>
2021-01-21 20:43:33 +0530

    updating the state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,26 +1,10 @@
-// Raising and handling events
-
-// The component that owns a piece of the state, should be the one modifying it.
-
-// Inorder to delete an item we need to remove it from the counters array in
-// counters component state object / property
-// We do not have access to the counters array in counter components state
-// Since the counters array is part of the counters component so modifying the
-// state should be done by counters component itself
-
-// But we know that state is something private and internal to a component
-// So inorder to modify the state of counters component from counter component
-// we have to raise an event in the counter component
-
-// counter component will raise an event and counters component will handle that event
-// we should implement onDelete in the counter to raise an event
-// we should implement handleDelete() in the counters component to handle that event
+// Updating the state
 
 import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value,
+    value: this.props.counter.value,
   };
 
   handleIncrement = () => {
@@ -32,20 +16,19 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement}
+          onClick={() => this.handleIncrement()}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
         <button
-          onClick={this.props.onDelete}
+          onClick={() => this.props.onDelete(this.props.counter.id)}
           className="btn btn-danger btn-sm m-2"
         >
           Delete
         </button>
       </div>
     );
-    // reference prop here 'this.props.onDelete'
   }
 
   getBadgeClasses() {
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,11 +11,17 @@ class Counters extends Component {
     ],
   };
 
-  // Event handler
-  handleDelete = () => {
-    console.log("Event handler called");
+  // to update the state we need to add counterId property so we know which
+  // counter we need to remove from the state
+
+  // in react we do not update the state directly, we are not going to remove
+  // a counter from the array instead we will create a new array without the given
+  // counter and call the setState method of our component and let react update
+  // the state
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
   };
-  // we want to pass a reference to this function using props to our child component
 
   render() {
     return (
@@ -24,8 +30,7 @@ class Counters extends Component {
           <Counter
             key={counter.id}
             onDelete={this.handleDelete}
-            value={counter.value}
-            id={counter.id}
+            counter={counter}
           />
         ))}
       </div>
